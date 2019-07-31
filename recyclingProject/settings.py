d29d93dab8bda935063b7f35aa19afb3bccb08f7
ouhmj7135027 <ouhmj7135027@naver.com>
2019-07-31 21:15:14 +0900

    second

--- recyclingProject/settings.py
+++ recyclingProject/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', ''.join([random.SystemRandom().
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
